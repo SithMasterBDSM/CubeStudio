0e9d4ecb9cbeab34d2d9973f9cfdf20ed12c92ec
SithMaster <jedilink@gmail.com>
2016-02-17 06:06:35 +0000

    0h10min: Estructura general del proyecto, iron-router

--- runWithExternalMongo.sh
+++ runWithExternalMongo.sh
@@ -1,2 +1,4 @@
-export MONGO_URL=mongodb://bogotasex.com:27017/bogotasex
-meteor run
+export HOSTNAME=`hostname`
+export ROOT_URL=http://www.cubestudio.co
+export MONGO_URL=mongodb://cubestudio.co:27017/cubestudio
+meteor --production --port 80 run
